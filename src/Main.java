2d1955d116306884fb1faa6528d864e02111a40d
michas_02 <michas_02@tlen.pl>
2015-03-24 15:53:33 +0100

    Automatyczna aktualizacja danych co określony czas

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		WeatherApp weather = new WeatherApp();
+		
 		try {
 			weather.updateData("Wroclaw");
 			System.out.println("Temperatura: "+Double.toString(weather.getWeatherInfo().getTemperature())
@@ -17,6 +18,8 @@ public class Main {
 			System.out.println("Nie udało się uzyskać informacji o pogodzie");
 			e.printStackTrace();
 		}
+		
+		weather.startAutoUpdate();
 		
 	}
 
--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -2,16 +2,75 @@ import java.io.IOException;
 
 
 public class WeatherApp {
+	private int sleepTime;
+	
+	private String cityName;
+	private int cityId;
+	private double cityLatitude;
+	private double cityLongitude;
+	
 	private String weatherInfoJSON = null;
 	private HttpClient client;
 	private WeatherParser parser;
 	private WeatherInfo weatherInfo;
+	
+	private UpdateDeamon deamon;
+	private boolean deamonRunning;
+	private boolean deamonPaused;
 	public WeatherApp()
 	{
+		deamon = new UpdateDeamon();
+		deamonRunning=true;
+		deamonPaused=true;
+		deamon.start();
+		sleepTime=1;
+		cityName="";
+		cityId=0;
+		cityLatitude=0;
+		cityLongitude=0;
 		client = new HttpClient();
 		parser = new WeatherParser();
 		weatherInfo = new WeatherInfo();
 		//weatherInfoJSON = client.getWeatherDataByCityName("Wroclaw");
+	}
+	
+	class UpdateDeamon extends Thread
+	{		
+		public void run()
+		{
+			while(deamonRunning)
+			{
+				if(deamonPaused)
+				{
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				else
+				{
+					try {
+						updateData();
+						System.out.println(weatherInfo.getTemperature()+","+weatherInfo.getHumidity()+","+weatherInfo.getWindSpeed());
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					for(int i=0;i<sleepTime;i++)
+					{
+						if(!deamonRunning||deamonPaused)
+							break;
+						try {
+							Thread.sleep(60*1000);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}
 	}
 	
 	public WeatherInfo getWeatherInfo()
@@ -28,20 +87,43 @@ public class WeatherApp {
 	}
 	public void updateData(int id) throws Exception
 	{
+		setCity(id);
 		weatherInfoJSON = client.getWeatherDataByCityId(id);
 		parseData();
 	}
 	
 	public void updateData(String name) throws Exception
 	{
+		setCity(name);
 		weatherInfoJSON = client.getWeatherDataByCityName(name);
 		parseData();
 	}
 	
 	public void updateData(double latitude,double longitude) throws Exception
 	{
+		setCity(latitude,longitude);
 		weatherInfoJSON = client.getWeatherDataByCoords(latitude, longitude);
 		parseData();
+	}
+	
+	public void updateData() throws Exception
+	{
+		if(cityId==0&&cityName.equals("")&&cityLatitude==0&&cityLongitude==0)
+		{
+			throw new Exception("Brak wcześniej zapisanych miast");
+		}
+		else if(cityId!=0)
+		{
+			updateData(cityId);
+		}
+		else if(!cityName.equals(""))
+		{
+			updateData(cityName);
+		}
+		else
+		{
+			updateData(cityLatitude,cityLongitude);
+		}
 	}
 	
 	private void parseData()
@@ -52,4 +134,43 @@ public class WeatherApp {
 		weatherInfo.setPressure(parser.getPressure(weatherInfoJSON));
 	}
 	
+	private void setCity(Object data)
+	{
+		cityId=0;
+		cityName="";
+		cityLatitude=0;
+		cityLongitude=0;
+		if(data instanceof Integer)
+		{
+			cityId=(int)data;
+		}
+		else if(data instanceof String)
+		{
+			cityName=(String)data;
+		}
+	}
+	private void setCity(double latitude, double longitude)
+	{
+		cityId=0;
+		cityName="";
+		cityLatitude=latitude;
+		cityLongitude=longitude;
+	}
+	
+	public void setSleepTime(int minutes)
+	{
+		this.sleepTime=minutes;
+	}
+	synchronized public void startAutoUpdate()
+	{
+		deamonPaused=false;
+	}
+	synchronized public void pausedAutoUpdate()
+	{
+		deamonPaused=true;
+	}
+	synchronized public void stopAutoUpdate()
+	{
+		deamonRunning=false;
+	}
 }
